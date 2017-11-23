12c316509d4fbc9fda5301a42cf18652376b6b56
ms <mrksauceda@gmail.com>
2017-11-23 18:36:06 +0800

    all code

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -5,10 +5,10 @@ class Ingredient < ApplicationRecord
   has_many :doses
   has_many :cocktails, through: :doses
 
-  before_destroy :check_if_any_doses
+  # before_destroy :check_if_any_doses
 
-  def check_if_any_doses
-    raise ActiveRecord::InvalidForeignKey if doses.any?
-  end
+  # def check_if_any_doses
+  #   raise ActiveRecord::InvalidForeignKey if doses.any?
+  # end
 
 end
